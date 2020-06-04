50bcdebe3380766043163ffc111e94092457ebf5
Maxim Lobanov <v-malob@microsoft.com>
2020-06-04 21:51:58 +0300

    Update setup.sh

--- temp/setup.sh
+++ temp/setup.sh
@@ -50,8 +50,8 @@ fi
 chmod +x ../python $PYTHON_MAJOR $PYTHON_MAJOR_DOT_MINOR $PYTHON_MAJORMINOR python
 
 echo "Upgrading PIP..."
-./python -m ensurepip
-./python -m pip install --ignore-installed pip
+#./python -m ensurepip
+#./python -m pip install --ignore-installed pip
 
 echo "Create complete file"
 touch $PYTHON_TOOLCACHE_VERSION_PATH/x64.complete
